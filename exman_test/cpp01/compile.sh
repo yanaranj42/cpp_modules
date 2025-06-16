cb4622a6a2f315896af0374a41fc3bb3ba5f2079
Yajaira <yajanaranjomey@gmail.com>
2025-06-16 18:40:04 +0200

    my test

--- exman_test/cpp01/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-c++ -Wall -Werror -Wextra -std=c++98 Warlock.hpp Warlock.cpp main.cpp \
-	ASpell.hpp ASpell.cpp ATarget.hpp ATarget.cpp \
-	Fwoosh.hpp Fwoosh.cpp \
-	Dummy.hpp Dummy.cpp
-
-./a.out
